09752e2fce80b872c2312783b11b3fb784c1ee79
Rag1rag1 <115003890+Rag1rag1@users.noreply.github.com>
2024-03-04 20:39:22 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(bool x1, bool x2, bool x3, bool x4) {
+    bool answer = true;
+    if((x1==false&&x2==false)||(x3==false&&x4==false))
+    {
+        answer= false;
+    }
+    return answer;
+}
