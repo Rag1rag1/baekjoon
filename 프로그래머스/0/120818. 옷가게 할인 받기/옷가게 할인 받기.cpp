95b6a89f728cd34444ba0018e7b562c79679c4ca
Rag1rag1 <115003890+Rag1rag1@users.noreply.github.com>
2024-03-06 20:52:56 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = price;
+    if(price>=100000&&price<300000)
+    {
+        answer*=0.95;
+    }
+    else if(price>=300000&&price<500000)
+    {
+        answer*=0.9;
+    }
+    else if(price>=500000){
+        answer*=0.8;
+    }
+    
+    return answer;
+}
